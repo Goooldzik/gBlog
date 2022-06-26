e8701ef5de58db3e8faf5d5df43a6a9716b2d318
Patrick <trybikpolski@gmail.com>
2022-06-27 00:12:12 +0200

    Initial commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,4 @@
-import _ from 'lodash';
-window._ = _;
+window._ = require('lodash');
 
 try {
     window.Popper = require('popper.js').default;
@@ -20,8 +19,7 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -33,8 +31,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+// window.Pusher = require('pusher-js');
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
