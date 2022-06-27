a14ecb120931635709014e8daeae52499cefe41b
Patrick <trybikpolski@gmail.com>
2022-06-27 03:36:09 +0200

    Bind contracts with classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repository\BaseRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\Contracts\BaseRepositoryContract;
+use App\Repository\Contracts\CategoryRepositoryContract;
+use App\Service\CategoryService;
+use App\Service\Contracts\CategoryServiceContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(BaseRepositoryContract::class, BaseRepository::class);
+
+        $this->app->bind(CategoryRepositoryContract::class, CategoryRepository::class);
+        $this->app->bind(CategoryServiceContract::class, CategoryService::class);
     }
 
     /**
